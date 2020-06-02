220424bf476f9d038b933d015c5879528551cd5b
Rohan <rhnmht30@gmail.com>
2020-06-02 21:14:55 +0530

    init: remove clutter add styled-components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+
+const StyledApp = styled.div`
+	display: flex;
+	flex-direction: column;
+	padding: 20px;
+	justify-content: center;
+	align-items: center;
+`;
+const StyledButton = styled.button`
+	border: ${(props) =>
+		props.type === "primary" ? "none" : "2px solid #a92456"};
+	border-radius: 5px;
+	background-color: ${(props) =>
+		props.type === "primary" ? "#e78e9e" : "#ffffff"};
+	color: #a92456;
+	padding: 10px;
+	font-size: 2em;
+	margin: 10px;
+
+	&:hover {
+		background-color: #a92456;
+		color: #e78e9e;
+	}
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<StyledApp className="App">
+			<StyledButton type="primary">Hi There!!</StyledButton>
+			<StyledButton type="secondary">Hi There!!</StyledButton>
+		</StyledApp>
+	);
 }
 
 export default App;
