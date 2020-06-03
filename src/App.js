76badcefb6d04c6cca3b178ec487c090424587fa
Rohan <rhnmht30@gmail.com>
2020-06-03 21:30:27 +0530

    Setup project

--- src/App.js
+++ src/App.js
@@ -1,37 +1,18 @@
-import React from "react";
-import styled from "styled-components";
+import React, { Component } from 'react';
 
-const StyledApp = styled.div`
-	display: flex;
-	flex-direction: column;
-	padding: 20px;
-	justify-content: center;
-	align-items: center;
-`;
-const StyledButton = styled.button`
-	border: ${(props) =>
-		props.type === "primary" ? "none" : "2px solid #a92456"};
-	border-radius: 5px;
-	background-color: ${(props) =>
-		props.type === "primary" ? "#e78e9e" : "#ffffff"};
-	color: #a92456;
-	padding: 10px;
-	font-size: 2em;
-	margin: 10px;
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
-	&:hover {
-		background-color: #a92456;
-		color: #e78e9e;
-	}
-`;
-
-function App() {
-	return (
-		<StyledApp className="App">
-			<StyledButton type="primary">Hi There!!</StyledButton>
-			<StyledButton type="secondary">Hi There!!</StyledButton>
-		</StyledApp>
-	);
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
